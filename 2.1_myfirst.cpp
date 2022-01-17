5e1253030a3be0924571f9cd8d912ebb7bc39717
Lai Li <laili_sclead@126.com>
2022-01-17 21:29:26 +0800

    2.1 myfirst.cpp

---
+++ 2.1_myfirst.cpp
@@ -0,0 +1,23 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-01-17 21:23:22
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-01-17 21:24:45
+ * @FilePath     : 2.1_myfirst.cpp
+ * @Description  : 
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    cout << "Come up and C++ my some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
