da5ad9a547f2cbd73fb56bd3028e476b13e16870
Lai Li <laili_sclead@126.com>
2022-01-20 23:27:39 +0800

    3.7 bondini.cpp

---
+++ ch3/example/3.7_bondini.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-01-20 23:15:00
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-01-20 23:16:22
+ * @FilePath     : 3.7_bondini.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    cout << "\aOperation \"HyperHype\" is now activated!" << endl;
+    cout << "Enter your agent code:________\b\b\b\b\b\b\b\b";
+    long code;
+    cin >> code;
+    cout << "\aYou entered " << code << "..." << endl;
+    cout << "\aCode verified! Proceed with Plan Z3!" << endl;
+
+    return 0;
+}
