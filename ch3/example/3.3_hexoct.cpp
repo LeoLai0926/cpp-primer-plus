209551bb8bbd0d118aef5f826445864146361e44
Lai Li <laili_sclead@126.com>
2022-01-20 22:50:55 +0800

    3.3 hexoct.cpp

---
+++ ch3/example/3.3_hexoct.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-01-20 22:47:48
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-01-20 22:49:44
+ * @FilePath     : 3.3_hexoct.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    int chest = 42;
+    int waist = 0x42;
+    int inseam = 042;
+
+    cout << "Monsieur cuts a striking figure!" << endl;
+    cout << "chest = " << chest << " (42 in decimal)" << endl;
+    cout << "waist = " << waist << " (0x42 in hex)" << endl;
+    cout << "inseam = " << inseam << " (042 in octal)" << endl;
+    return 0;
+}
