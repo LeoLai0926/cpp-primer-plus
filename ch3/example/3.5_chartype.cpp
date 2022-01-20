32d3cfe272961c77bcebb4cfd6224fd3bc06e33d
Lai Li <laili_sclead@126.com>
2022-01-20 23:07:03 +0800

    3.5 chartype.cpp

---
+++ ch3/example/3.5_chartype.cpp
@@ -0,0 +1,27 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-01-20 22:59:12
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-01-20 23:00:13
+ * @FilePath     : 3.5_chartype.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    char ch;
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hola! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+
+    return 0;
+}
