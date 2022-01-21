30e4f7310128f146e1c88efee45ae7657fd8e049
Lai Li <laili_sclead@126.com>
2022-01-21 21:06:24 +0800

    3.9 fltadd.cpp

---
+++ ch3/example/3.9_fltadd.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-01-21 21:04:00
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-01-21 21:04:01
+ * @FilePath     : 3.9_fltadd.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    float a = 2.34E+22f;
+    float b = a + 1.0f;
+    cout << "a = " << a << endl;
+    cout << "b - a = " << b - a << endl;
+    return 0;
+}
