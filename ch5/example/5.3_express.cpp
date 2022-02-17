fad22330970760bb717e052620f19dc656fb241b
Lai Li <laili_sclead@126.com>
2022-02-17 19:43:19 +0800

    5.3 express.cpp

---
+++ ch5/example/5.3_express.cpp
@@ -0,0 +1,34 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-02-17 19:39:39
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-02-17 19:43:05
+ * @FilePath     : 5.3_express.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    int x;
+
+    cout << "The expression x = 100 has the value ";
+    cout << (x = 100) << endl;
+    cout << "Now x = " << x << endl;
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    cout.setf(ios_base::boolalpha); // a newer C++ feature
+    cout << "The expression x < 3 has the value ";
+    cout << (x < 3) << endl;
+    cout << "The expression x > 3 has the value ";
+    cout << (x > 3) << endl;
+    return 0;
+}
