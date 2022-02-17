2d577451d484e88f5bcee5c6a937877f0f9910a1
Lai Li <laili_sclead@126.com>
2022-02-17 19:36:54 +0800

    5.1 forloop.cpp

---
+++ ch5/example/5.1_forloop.cpp
@@ -0,0 +1,25 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-02-17 19:34:47
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-02-17 19:36:00
+ * @FilePath     : 5.1_forloop.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    int i;              // create a counter
+    // initialize; test; update
+    for (i = 0; i < 5; i++)
+        cout << "C++ knows loops." << endl;
+    cout << "C++ knows when to stop." << endl;
+    return 0;
+}
