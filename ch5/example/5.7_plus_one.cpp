0bff0ab07fafa4c2eb90c4d78a935b6254e34c7c
Lai Li <laili_sclead@126.com>
2022-02-18 20:41:51 +0800

    5.7 plus_one.cpp

---
+++ ch5/example/5.7_plus_one.cpp
@@ -0,0 +1,26 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-02-17 20:00:00
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-02-17 20:08:57
+ * @FilePath     : 5.7_plus_one.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    using namespace std;
+    int a = 20;
+    int b = 20;
+    cout << "a      = " << a << " :     b = " << b << endl;
+    cout << "a++    = " << a++ << " :   ++b = " << ++b << endl;
+    cout << "a      = " << a << " :     b = " << b << endl;
+    return 0;
+}
