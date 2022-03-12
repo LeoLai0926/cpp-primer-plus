64d9640950378be842aa8d5f6cd247f564eb8945
Lai Li <laili_sclead@126.com>
2022-03-12 21:52:13 +0800

    ch7 ex8.cpp

---
+++ ch7/exercise/ex8a.cpp
@@ -0,0 +1,57 @@
+/*
+ * @Author       : Lai Li
+ * @E-mail       : laili_sclead@126.com
+ * @Department   : Image Algorithm Department
+ * @Date         : 2022-03-12 21:45:30
+ * @LastEditors  : Lai Li
+ * @LastEditTime : 2022-03-12 21:49:21
+ * @FilePath     : ex8.cpp
+ * @Description  :
+ * Copyright (c) 2022 Chengdu Lead Science&Technology Co.,Ltd.,
+ * All Rights Reserved.
+ */
+
+#include <iostream>
+#include <string>
+
+// constant data
+const int SEASONS = 4;
+const char * SNAMES[] =
+    {"Spring", "Summer", "Fall", "Winter"};
+
+// function to modify array object
+void fill(double arr[], int size);
+// function that uses array object without modifying it
+void show(double arr[], int size);
+
+int main(int argc, char const *argv[])
+{
+    double expenses[SEASONS];
+    fill(expenses, SEASONS);
+    show(expenses, SEASONS);
+    return 0;
+}
+
+void fill(double arr[], int size)
+{
+    using namespace std;
+    for (int i = 0; i < size; i++)
+    {
+        cout << "Enter " << SNAMES[i] << " expenses: ";
+        cin >> arr[i];
+    }
+}
+
+void show(double arr [], int size)
+{
+    using namespace std;
+    double total = 0.0;
+    cout << endl
+         << "EXPENSES" << endl;
+    for (int i = 0; i < size; i++)
+    {
+        cout << SNAMES[i] << ": $" << arr[i] << endl;
+        total += arr[i];
+    }
+    cout << "Total Expenses: $" << total << endl;
+}
